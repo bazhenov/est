70f9e91abd83af393341d6478fa2edeffb770546
Denis Bazhenov <bazhenov@farpost.com>
2015-01-03 15:41:12 +1000

    changes in install script

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ OUT_FILE=/usr/local/bin/est.jar
 curl -o $OUT_FILE https://dl.dropboxusercontent.com/u/16254496/est.jar
 echo "#\!/bin/sh
 java -jar /usr/local/bin/est.jar \$@" > /usr/local/bin/est
+chmod a+x /usr/local/bin/est
